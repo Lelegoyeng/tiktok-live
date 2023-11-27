5fbf02dff4d43e1c47bc66c5349f151b749a9c1f
lelegoyeng <lelegoyengs@gmail.com>
2023-11-27 11:35:23 +0700

    Update

--- bot-ubuntu/bot-windows.js
+++ bot-ubuntu/bot-windows.js
@@ -3,14 +3,20 @@ require('dotenv').config();
 
 const BOT = async () => {
     const browser = await puppeteer.launch({
-        defaultViewport: { width: 1080, height: 1024 },
+        defaultViewport: { width: 1920, height: 1080 },
         headless: false
     });
     const page = await browser.newPage();
     await page.goto('https://accounts.google.co.id/');
-    await page.type('#identifierId', process.env.gmail);
-    await page.screenshot({ path: 'screenshot.png' });
+    await page.waitForSelector('input[type="email"]');
+    await page.focus('input[type="email"]');
+    await page.keyboard.type(process.env.gmail, { delay: 50 });
+    await page.click('#identifierNext');
 
+    await page.waitForSelector('input[type="password"]');
+    await page.focus('input[type="password"]');
+    await page.keyboard.type(process.env.password, { delay: 50 });
+    await page.click('#passwordNext');
     // const searchResultSelector = '.search-box__link';
     // await page.waitForSelector(searchResultSelector);
     // await page.click(searchResultSelector);
