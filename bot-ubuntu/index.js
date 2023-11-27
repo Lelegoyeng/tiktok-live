b3873f8380bba3463d8bac3e64a625f84913b062
lelegoyeng <lelegoyengs@gmail.com>
2023-11-27 11:28:05 +0700

    Update

--- bot-ubuntu/index.js
+++ bot-ubuntu/index.js
@@ -2,29 +2,28 @@ const puppeteer = require('puppeteer');
 require('dotenv').config();
 
 const BOT = async () => {
-    const browser = await puppeteer.launch({
-        defaultViewport: {width: 1920, height: 1080}
-      });
-    const page = await browser.newPage();
-    await page.goto('https://accounts.google.co.id/');
-    await page.setViewport({ width: 1080, height: 1024 });
-    await page.type('#identifierId', process.env.gmail);
-    await page.screenshot({path: 'screenshot.png'});
+  const browser = await puppeteer.launch({
+    defaultViewport: { width: 1080, height: 1024 }
+  });
+  const page = await browser.newPage();
+  await page.goto('https://accounts.google.co.id/');
+  await page.type('#identifierId', process.env.gmail);
+  await page.screenshot({ path: 'screenshot.png' });
 
-    // const searchResultSelector = '.search-box__link';
-    // await page.waitForSelector(searchResultSelector);
-    // await page.click(searchResultSelector);
+  // const searchResultSelector = '.search-box__link';
+  // await page.waitForSelector(searchResultSelector);
+  // await page.click(searchResultSelector);
 
-    // Locate the full title with a unique string
-    // const textSelector = await page.waitForSelector(
-    //     'text/Customize and automate'
-    // );
-    // const fullTitle = await textSelector?.evaluate(el => el.textContent);
+  // Locate the full title with a unique string
+  // const textSelector = await page.waitForSelector(
+  //     'text/Customize and automate'
+  // );
+  // const fullTitle = await textSelector?.evaluate(el => el.textContent);
 
-    // // Print the full title
-    // console.log('The title of this blog post is "%s".', fullTitle);
+  // // Print the full title
+  // console.log('The title of this blog post is "%s".', fullTitle);
 
-    // await browser.close();
+  // await browser.close();
 }
 
 BOT();
